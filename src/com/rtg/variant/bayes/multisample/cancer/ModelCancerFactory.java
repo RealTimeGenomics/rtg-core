fea169837dfd4265ec1c2b0c49991154b098f861
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2015-02-09 10:13:16 +1300

    Fix for Bug#1593, NPE in somatic caller when ref=N

--- src/com/rtg/variant/bayes/multisample/cancer/ModelCancerFactory.java
+++ src/com/rtg/variant/bayes/multisample/cancer/ModelCancerFactory.java
@@ -37,6 +37,8 @@ public class ModelCancerFactory extends ModelCommonFactory<Description, Hypothes
   public ModelCancerFactory(final GenomePriorParams params, final double contamination, final boolean haploid) {
     super();
     mContamination = contamination;
+    final HypothesesSnp unknownHypothesesSnp = new HypothesesSnp(LogApproximatePossibility.SINGLETON, params, haploid, -1);
+    mHypothesisUnknown = new HypothesesCancer(unknownHypothesesSnp, LogApproximatePossibility.SINGLETON);
     for (int i = 0; i < DescriptionSnp.SINGLETON.size(); i++) {
       final HypothesesSnp hyp = new HypothesesSnp(SimplePossibility.SINGLETON, params, haploid, i);
       mHypothesesCache.add(new HypothesesCancer(hyp, LogApproximatePossibility.SINGLETON));
