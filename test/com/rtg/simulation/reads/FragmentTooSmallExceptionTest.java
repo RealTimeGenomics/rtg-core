d344e743a6f0fac70c3389b9ca0958cb3996b5e9
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2015-12-02 11:40:05 +1300

    Add test for FragmentTooSmallException

---
+++ test/com/rtg/simulation/reads/FragmentTooSmallExceptionTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2014. Real Time Genomics Limited.
+ *
+ * Use of this source code is bound by the Real Time Genomics Limited Software Licence Agreement
+ * for Academic Non-commercial Research Purposes only.
+ *
+ * If you did not receive a license accompanying this file, a copy must first be obtained by email
+ * from support@realtimegenomics.com.  On downloading, using and/or continuing to use this source
+ * code you accept the terms of that license agreement and any amendments to those terms that may
+ * be made from time to time by Real Time Genomics Limited.
+ */
+
+package com.rtg.simulation.reads;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests the corresponding class.
+ */
+public class FragmentTooSmallExceptionTest extends TestCase {
+
+  public void test() {
+    final FragmentTooSmallException e = new FragmentTooSmallException(42, 43);
+    assertEquals("Fragment length (42) was not large enough to accomodate read of length 43 after indels, try increasing fragment length", e.getMessage());
+  }
+}
