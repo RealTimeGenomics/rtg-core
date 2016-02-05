ce360beea7a8c264663a342b7a04eb6c4e046c24
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2016-02-05 17:02:50 +1300

    Reduce number of log/lgamma calls in allele balance calculation and correct behaviour for p!=0.5

--- src/com/rtg/variant/bayes/BinomialAlleleBalance.java
+++ src/com/rtg/variant/bayes/BinomialAlleleBalance.java
@@ -27,22 +27,35 @@ public class BinomialAlleleBalance extends AbstractAlleleBalance {
     super(expected);
   }
 
-  private static double logBinomial(final double p, final double nn, final double n) {
+  // C(total, count) * p^count * (1-p)^(total-count)
+//  private static double logBinomial(final double p, final double total, final double count) {
+//    assert p >= 0.0 && p <= 1.0;
+//    assert count >= 0;
+//    assert total >= 0;
+//    final double m = total - count;
+//    final double res = count * Math.log(p) + m * Math.log(1.0 - p) + ChiSquared.lgamma(total + 1) - ChiSquared.lgamma(count + 1) - ChiSquared.lgamma(m + 1);
+//    assert res <= 0 && !Double.isNaN(res);
+//    return res;
+//  }
+
+  // Compute a probability in log space using a binomial style distribution for the probability of seeing
+  // count1/total having expected probability p and count2/total having expected probability 1-p
+  private static double logBinomial(final double p, final double total, final double count1, final double count2) {
     assert p >= 0.0 && p <= 1.0;
-    assert n >= 0;
-    assert nn >= 0;
-    final double m = nn - n;
-    final double res = n * Math.log(p) + m * Math.log(1.0 - p) + ChiSquared.lgamma(nn + 1) - ChiSquared.lgamma(n + 1) - ChiSquared.lgamma(m + 1);
+    assert count1 >= 0;
+    assert count2 >= 0;
+    assert total >= 0;
+    final double res = (count1 + total - count2) * Math.log(p) + (total - count1 + count2) * Math.log(1.0 - p)
+      + 2 * ChiSquared.lgamma(total + 1)
+      - ChiSquared.lgamma(count1 + 1) - ChiSquared.lgamma(total - count1 + 1)
+      - ChiSquared.lgamma(count2 + 1) - ChiSquared.lgamma(total - count2 + 1);
     assert res <= 0 && !Double.isNaN(res);
     return res;
   }
 
   @Override
   double alleleBalanceHeterozygousLn(double p, double trials, double count, double countAlt) {
-    return alleleBalanceLn(p, trials, count) + alleleBalanceLn(p, trials, countAlt);
-  }
-
-  private double alleleBalanceLn(double p, double trials, double count) {
-    return logBinomial(p, trials, count);
+    // Compute bin(p, trials, count) * bin(1 - p, trials, countAlt)
+    return logBinomial(p, trials, count, countAlt);
   }
 }
