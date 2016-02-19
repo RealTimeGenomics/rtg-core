c9794fefd8b6be253b664016df51aee73a6432f2
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2016-02-19 16:35:16 +1300

    findbugs: Se, remove redundant import

--- src/com/rtg/variant/sv/discord/pattern/DeletionOverlapFilter.java
+++ src/com/rtg/variant/sv/discord/pattern/DeletionOverlapFilter.java
@@ -17,7 +17,6 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
