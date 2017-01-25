218ce218174d8539abce7372816f712a5da650cc
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2017-01-25 14:18:58 +1300

    Another minimal of PON creation

--- test/com/rtg/variant/cnv/segment/CnvPonBuildCliTest.java
+++ test/com/rtg/variant/cnv/segment/CnvPonBuildCliTest.java
@@ -12,10 +12,16 @@
 
 package com.rtg.variant.cnv.segment;
 
+import java.io.File;
 import java.io.IOException;
 
 import com.rtg.launcher.AbstractCli;
 import com.rtg.launcher.AbstractCliTest;
+import com.rtg.launcher.MainResult;
+import com.rtg.reader.ReaderTestUtils;
+import com.rtg.util.StringUtils;
+import com.rtg.util.io.TestDirectory;
+import com.rtg.util.test.FileHelper;
 
 /**
  * Tests the corresponding class.
@@ -31,7 +37,11 @@ public class CnvPonBuildCliTest extends AbstractCliTest {
     checkHelp("Construct a normalized coverage sample from a panel of coverage outputs.",
       "BED output file",
       "SDF containing reference genome",
-      "coverage BED file. Must be specified 1 or more times"
+      "coverage BED file. Must be specified 1 or more times",
+      "do not gzip the output",
+      "do not produce indexes for output files",
+      "Utility",
+      "File Input/Output"
       );
   }
 
@@ -40,6 +50,21 @@ public class CnvPonBuildCliTest extends AbstractCliTest {
     final String exp = getCFlags().getUsageHeader();
     assertTrue(res.contains(exp));
     assertTrue(res.contains("Error: You must provide values for -o FILE -t SDF FILE+"));
+  }
+
+  public void testValid() throws IOException {
+    try (TestDirectory dir = new TestDirectory()) {
+      final File coverage = FileHelper.resourceToFile("com/rtg/variant/cnv/segment/resources/coverage.bed", new File(dir, "coverage.bed"));
+      final File output = new File(dir, "panel.bed");
+      // Slightly G rich reference so G+C normalization actually does something
+      final String ref = ">1" + System.getProperty("line.separator") + StringUtils.repeat("ACGGT", 25000 / "ACGGT".length());
+      final File sdf = new File(dir, "sdf");
+      ReaderTestUtils.getDNADir(ref, sdf);
+      final MainResult result = checkMainInit("-Z", "-o", output.getPath(), "-t", sdf.getPath(), coverage.getPath());
+      final String actual = StringUtils.grepMinusV(FileHelper.fileToString(output), "^#");
+      mNano.check("expected.panel.bed", actual);
+      mNano.check("expected.out.txt", result.out().replaceAll("/[^ ]*/", ""));
+    }
   }
 
 }
