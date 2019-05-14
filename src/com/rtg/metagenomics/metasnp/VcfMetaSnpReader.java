7288297aa22bc2481e743359849a9d214a3658cd
Len Trigg <len@realtimegenomics.com>
2019-05-15 11:31:29 +1200

    Making a factory for VcfReader construction.

--- src/com/rtg/metagenomics/metasnp/VcfMetaSnpReader.java
+++ src/com/rtg/metagenomics/metasnp/VcfMetaSnpReader.java
@@ -12,39 +12,50 @@
 package com.rtg.metagenomics.metasnp;
 
 import java.io.BufferedReader;
+import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import com.rtg.util.io.FileUtils;
 import com.rtg.vcf.VcfReader;
+import com.rtg.vcf.VcfReaderFactory;
 
 /**
  * Adapter to convert from VCF input into records for metasnp.
  */
-class VcfMetaSnpReader extends VcfReader implements MetaSnpReader {
+class VcfMetaSnpReader implements MetaSnpReader {
+
+  private final VcfReader mReader;
 
   VcfMetaSnpReader(final File in) throws IOException {
-    this(new BufferedReader(FileUtils.createReader(in, false)));
+    mReader = new VcfReaderFactory().make(in);
   }
 
   VcfMetaSnpReader(final BufferedReader in) throws IOException {
-    super(in);
+    mReader = new VcfReaderFactory().make(in);
   }
 
   @Override
   public List<String> samples() {
-    return getHeader().getSampleNames();
+    return mReader.getHeader().getSampleNames();
   }
 
   @Override
   public MetaSnpLine nextLine() throws IOException {
-    while (hasNext()) {
-      final MetaSnpLine metaSnpLine = MetaSnpLine.create(next());
+    while (mReader.hasNext()) {
+      final MetaSnpLine metaSnpLine = MetaSnpLine.create(mReader.next());
       if (metaSnpLine != null) {
         return metaSnpLine;
       }
     }
     return null;
+  }
+
+  @Override
+  @SuppressWarnings("try")
+  public void close() throws IOException {
+    try (Closeable ignored = mReader) {
+      // used for nice closing behaviour
+    }
   }
 }
