2abf7fc3710fb8e11041014ded32c2d973f26da6
Sean A. Irvine <sean.irvine@realtimegenomics.com>
2024-11-07 10:46:40 +1300

    Migrate rtg-core to BSD license

--- src/main/java/com/rtg/launcher/globals/CoreGlobalFlags.java
+++ src/main/java/com/rtg/launcher/globals/CoreGlobalFlags.java
@@ -1,13 +1,31 @@
 /*
- * Copyright (c) 2014. Real Time Genomics Limited.
+ * Copyright (c) 2018. Real Time Genomics Limited.
  *
- * Use of this source code is bound by the Real Time Genomics Limited Software Licence Agreement
- * for Academic Non-commercial Research Purposes only.
+ * All rights reserved.
  *
- * If you did not receive a license accompanying this file, a copy must first be obtained by email
- * from support@realtimegenomics.com.  On downloading, using and/or continuing to use this source
- * code you accept the terms of that license agreement and any amendments to those terms that may
- * be made from time to time by Real Time Genomics Limited.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package com.rtg.launcher.globals;
 
